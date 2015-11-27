00b20ec83b5f2b05668e98b2dd7f1c8f8af690cc
Desiree <desireepoland@gmail.com>
2015-11-27 15:27:22 -0800

    Add ability to add a new stop to a trip

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -6,4 +6,32 @@ class StopsController < ApplicationController
     @activities = @stop.activities
   end
 
+  def new
+    @stop = Stop.new
+    @header = "Add A New Stop"
+    @action = "create"
+    @method = :post
+  end
+
+  def edit
+  end
+
+  def create
+    trip = Trip.find(params[:trip_id])
+    trip.stops.create(stop_params)
+    redirect_to trip_path(params[:trip_id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def stop_params
+    params.require(:stop).permit(:name, :description)
+  end
+
 end

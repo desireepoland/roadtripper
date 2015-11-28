2a8833efc1deeb46a4be35097d191b76212393b4
Desiree <desireepoland@gmail.com>
2015-11-27 16:18:27 -0800

    Add ability to create activities for a stop

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,22 @@
 class ActivitiesController < ApplicationController
+
+  def new
+    @activity = Activity.new
+    @header = "Add A New Activity"
+    @action = "create"
+    @method = :post
+  end
+
+  def create
+    stop = Stop.find(params[:stop_id])
+    stop.activities.create(activity_params)
+    redirect_to trip_stop_path(stop.trip_id, stop.id)
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name)
+  end
+
 end

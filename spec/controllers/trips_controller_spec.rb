ca123b9f4aef042b7ad1c1e5df16b53495b262ca
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-28 17:12:16 -0800

    added trip specs for show and new

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe TripsController, type: :controller do
 
+  let(:trip) do
+    Trip.create(name: "Road Trip", description: "This is gonna be awesome")
+  end
+
   describe "GET #home" do
     it "renders the home view" do
       get :home
@@ -13,6 +17,20 @@ RSpec.describe TripsController, type: :controller do
     it "renders the index view" do
       get :index
       expect(subject).to render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    it "renders the show view" do
+      get :show, id: trip.id
+      expect(subject).to render_template :show
+    end
+  end
+
+  describe "GET #new" do
+    it "renders the new view" do
+      get :new
+      expect(subject).to render_template :form
     end
   end
 

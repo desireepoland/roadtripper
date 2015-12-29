ab0b686dfc68073f6e8d7e93da5c30ab1165e27e
Annalee <annaleeherrera@gmail.com>
2015-12-29 13:52:04 -0800

    added validations for name and description in trip model.  All tests passing.

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-require 'pry'
+
 RSpec.describe Trip, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   let(:good_hash) do
@@ -21,7 +21,7 @@ RSpec.describe Trip, type: :model do
  it "must have a name" do
     expect(Trip.new(good_hash)).to be_valid
     expect(Trip.new(no_name_hash)).to_not be_valid
-  end
+end
 
   it "must have a description" do
      expect(Trip.new(good_hash)).to be_valid

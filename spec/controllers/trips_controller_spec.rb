0a6650feb19cb9463a8ef9bba4a57d8711898213
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-28 17:05:44 -0800

    added trip specs for home and index views

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe TripsController, type: :controller do
 
+  describe "GET #home" do
+    it "renders the home view" do
+      get :home
+      expect(subject).to render_template :home
+    end
+  end
+
+  describe "GET #index" do
+    it "renders the index view" do
+      get :index
+      expect(subject).to render_template :index
+    end
+  end
+
 end
